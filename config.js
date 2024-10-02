70f77e37650dcdf9d0ccdd15a5f1a130330da3e2
khoa nek <127484867+minhkhoanek193@users.noreply.github.com>
2024-10-02 15:43:10 +0700

    Add files via upload
    tao

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em';
+const giftUrl = '';
+const eventName = 'Happy birthday';
+const titleCard = 'Gift em Tong';
+const contentCard = 'Happy Birthday, my love! Even though we are miles apart, my heart is always with you. Every moment without you reminds me just how much you mean to me. I cant wait for the day we can celebrate together. Until then, know that Im sending you all my love across the distance. You are my everything, and today, I celebrate you. Happy birthday, my forever!';
+
+const giftImage = 'em.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
